ca8a9b81c60122dd36f52324dfa644eac7e45cf9
VadimRight <vadimright@mail.ru>
2024-01-28 19:12:35 +0300

    build: usual register is done

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -17,9 +17,6 @@ class UserCreate(schemas.BaseUserCreate):
     email: EmailStr
     password: str
     username: str
-    is_active: Optional[bool] = True
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
 
 
 class UserUpdate(schemas.BaseUserUpdate):
