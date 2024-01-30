226d13e512773aba8c29038003cc35ad2e8f29d1
VadimRight <vadimright@mail.ru>
2024-01-30 23:51:11 +0300

    Fit: Model for user is upgraded and migration is done

--- src/models/models.py
+++ src/models/models.py
@@ -1,8 +1,10 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy_imageattach.entity import image_attachment
 
 from src.database import Base, metadata
 
-from sqlalchemy import Column, Integer, Text, MetaData, String, create_engine, LargeBinary, ForeignKey
+from sqlalchemy import Column, Integer, Text, MetaData, String, create_engine, LargeBinary, ForeignKey, Boolean
 from sqlalchemy import (
     Integer,
     String,
@@ -18,11 +20,27 @@ class Image(Base):
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
 
-class User(Base):
-    id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False, unique=True)
-    hashed_password: str = Column(String(length=24), nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    __tablename__ = 'user'
+# class User(Base):
+#     id = Column(Integer, primary_key=True)
+#     username = Column(String, nullable=False, unique=True)
+#     hashed_password: str = Column(String(length=24), nullable=False)
+#     email = Column(String, nullable=False, unique=True)
+#     __tablename__ = 'user'
 
 
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(
+        String(length=320), unique=True, index=True, nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        String(length=1024), nullable=False
+    )
+    username: Mapped[str] = mapped_column(String(length=20), nullable=False, unique=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
