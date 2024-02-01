5d9ea7423179f1fc6ff69fccea6223b2880186ce
VadimRight <vadim.komashko@bk.ru>
2024-02-01 19:18:18 +0300

    Temp

--- src/auth/crud.py
+++ src/auth/crud.py
@@ -98,23 +98,23 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], sessio
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except jwt.PyJWTError:
-        raise credentials_exception
-    user = await get_user_by_id(session, username=token_data.username)
-    if user is None:
-        raise credentials_exception
+    # try:
+    payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+    email: str = payload.get("sub")
+    # if email is None:
+    #     raise credentials_exception
+    token_data = TokenData(email=email)
+    # except jwt.PyJWTError:
+    #     raise credentials_exception
+    user = await get_user_by_email(session, email=token_data.email)
+    # if user is None:
+    #     raise credentials_exception
     return user
 
 
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+# async def get_current_active_user(
+#     current_user: Annotated[User, Depends(get_current_user)]
+# ):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
--- src/auth/router.py
+++ src/auth/router.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import Session
 from starlette import status
 from starlette.status import HTTP_401_UNAUTHORIZED
 
-from src.auth.crud import create_user, get_user_by_email, get_user_by_id, get_user_by_username, get_current_active_user, \
-    authenticate_user, create_access_token
+from src.auth.crud import create_user, get_user_by_email, get_user_by_id, get_user_by_username,  \
+    authenticate_user, create_access_token, get_current_user
 from src.auth.oauth import oauth2_scheme
 from src.auth.schemas import UserSchema, CreateUserSchema, UserLoginSchema, UserBaseSchema, Token
 from src.config import ACCESS_TOKEN_EXPIRE_MINUTES
@@ -25,24 +25,24 @@ router = APIRouter(
 )
 
 
-# @router.post('/login', response_model=Dict)
-# async def signup(
-#         payload: OAuth2PasswordRequestForm = Depends(),
-#         session: AsyncSession = Depends(get_session)
-# ):
-#     user: User = await get_user_by_email(session=session, email=payload.username)
-#     if user is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail="Invalid user credentials"
-#         )
-#     is_validated: bool = user.validate_password(payload.password)
-#     if not is_validated:
-#         raise HTTPException(
-#             status_code=HTTP_401_UNAUTHORIZED,
-#             detail="Invalid user credentials"
-#         )
-#     return user.generate_token()
+@router.post('/login', response_model=Dict)
+async def signup(
+        payload: OAuth2PasswordRequestForm = Depends(),
+        session: AsyncSession = Depends(get_session)
+):
+    user: User = await get_user_by_email(session=session, email=payload.username)
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid user credentials"
+        )
+    is_validated: bool = user.validate_password(payload.password)
+    if not is_validated:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Invalid user credentials"
+        )
+    return user.generate_token()
 
 
 @router.get("/profile/{username}")
@@ -64,25 +64,25 @@ async def register(payload: CreateUserSchema = Body(),
 
 @router.get("/users/me/", response_model=UserSchema)
 async def read_users_me(
-    current_user: Annotated[UserSchema, Depends(get_current_active_user)]
+    current_user: Annotated[UserSchema, Depends(get_current_user)]
 ):
     return current_user
 
 
-@router.post("/token")
-async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: AsyncSession = Depends(get_session)
-) -> Token:
-    user = await authenticate_user(session, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
+# @router.post("/token")
+# async def login_for_access_token(
+#     form_data: OAuth2PasswordRequestForm = Depends(),
+#     session: AsyncSession = Depends(get_session)
+# ) -> Token:
+#     user = await authenticate_user(session, form_data.username, form_data.password)
+#     # if not user:
+#     #     raise HTTPException(
+#     #         status_code=status.HTTP_401_UNAUTHORIZED,
+#     #         detail="Incorrect username or password",
+#     #         headers={"WWW-Authenticate": "Bearer"},
+#     #     )
+#     access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+#     access_token = create_access_token(
+#         data={"sub": user.username}, expires_delta=access_token_expires
+#     )
+#     return Token(access_token=access_token, token_type="bearer")
