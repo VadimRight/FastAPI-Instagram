b9a6e04d0418386e2d0402b250e381bdfcc86475
VadimRight <vadim.komashko@bk.ru>
2024-02-06 12:43:13 +0300

    Removed try and exept from get current user

--- src/auth/crud.py
+++ src/auth/crud.py
@@ -81,15 +81,15 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], sessio
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except jwt.PyJWTError:
-        raise credentials_exception
+    # try:
+    payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+    username: str = payload.get("sub")
+        # if username is None:
+        #     raise credentials_exception
+    token_data = TokenData(username=username)
+    # except jwt.PyJWTError:
+    #     raise credentials_exception
     user: User = await get_user_by_username(session, username=token_data.username)
-    if user is None:
-        raise credentials_exception
+    # if user is None:
+    #     raise credentials_exception
     return user
