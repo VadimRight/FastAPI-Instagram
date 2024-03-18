cbb36667c020d0c97c1225c8cf4c0511b33acf73
VadimRight <vadimright@mail.ru>
2024-03-19 02:13:15 +0300

    all endpoints are on

--- src/post/router.py
+++ src/post/router.py
@@ -25,7 +25,7 @@ async def get_image(username: str, session: AsyncSession =  Depends(get_session)
 
 
 @router.get("/users/{username}/posts={id}")
-async def get_spesfic_post(username: str, id: int, session: AsyncSession = Depends(get_session)):
+async def get_spesfic_post(username: str, id: str, session: AsyncSession = Depends(get_session)):
     await get_user_by_username(session, username)
     post: Post = await get_post_by_id(session, id)
     return post
@@ -43,7 +43,7 @@ async def get_my_images(session: AsyncSession = Depends(get_session), token: str
 
 
 @router.get("/profile/posts={id}")
-async def get_spesfic_post(username: str, id: int, session: AsyncSession = Depends(get_session)):
+async def get_spesfic_post(username: str, id: str, session: AsyncSession = Depends(get_session)):
     await get_user_by_username(session, username)
     post: Post = await get_post_by_id(session, id)
     return post
@@ -51,15 +51,15 @@ async def get_spesfic_post(username: str, id: int, session: AsyncSession = Depen
 
 
 @router.delete("/profile/posts={id}")
-async def delete_image( id: int, session: AsyncSession = Depends(get_session), token = Depends(oauth2_scheme)):
+async def delete_image( id: str, session: AsyncSession = Depends(get_session), token = Depends(oauth2_scheme)):
     return await delete_my_post(session, id, token)
 
 
 @router.patch("/profile/posts={id}/edit_name")
-async def update_name(id: int, name: str, session: AsyncSession = Depends(get_session), token = Depends(oauth2_scheme)):
+async def update_name(id: str, name: str, session: AsyncSession = Depends(get_session), token = Depends(oauth2_scheme)):
     return await edit_post_name(session, id, name, token)
 
 
 @router.patch("/profile/posts={id}/edit_image")
-async def upgrade_image(id: int, image: str, session: AsyncSession = Depends(get_session), token = Depends(oauth2_scheme)):
+async def upgrade_image(id: str, image: str, session: AsyncSession = Depends(get_session), token = Depends(oauth2_scheme)):
     return await edit_post_image(session, id, image, token)
