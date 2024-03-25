74732cd56393f54e48885d5231e8212bba025809
VadimRight <vadimright@mail.ru>
2024-03-25 15:23:49 +0300

    Fit: like for comment business

--- src/like_comment/crud.py
+++ src/like_comment/crud.py
@@ -4,22 +4,22 @@ from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_session
-from src.like_post.schemas import LikePostSchema
-from src.models.models import Like_For_Post
+from src.like_post.schemas import LikeCommentSchema
+from src.models.models import Like_For_Comment
 from src.verif import get_id_from_token
 from src.auth.oauth import oauth2_scheme
 
 async def create_like_post(post_id, session: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)):
     async with session.begin():
         user_id = get_id_from_token(token)
-        like_for_post = Like_For_Post(id = uuid4, user_id = user_id, post_id = post_id)
+        like_for_post = Like_For_Comment(id = uuid4, user_id = user_id, post_id = post_id)
         session.add(like_for_post)
         await session.flush
         await session.refresh(like_for_post)
-        return LikePostSchema.model_validate(like_for_post)
+        return LikeCommentSchema.model_validate(like_for_post)
     
 
 async def delete_like_post(post_id, session: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)):
     async with session.begin():
-        query = delete(Like_For_Post).where(post_id == post_id)
+        query = delete(Like_For_Comment).where(post_id == post_id)
         await session.execute(query)
--- src/like_post/schemas.py
+++ src/like_post/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import UUID4, BaseModel
 
 
-class LikeSchema(BaseModel):
+class LikePostSchema(BaseModel):
     id:  UUID4
     user_id: UUID4
     post_id: UUID4
