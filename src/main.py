d30208175da9615d10d20d852947ccd9beadb311
VadimRight <vadimright@mail.ru>
2024-03-25 13:37:41 +0300

    deleated all comment and like reouters

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 from src.post.router import router as image_router
 from src.auth.router import router as user_router
-from src.comment.router import router as comment_router
 # FastAPI App initialization
 app = FastAPI(
     title='Instagram FastAPI'
@@ -11,4 +10,3 @@ app = FastAPI(
 # Post and User Routers are included in app
 app.include_router(image_router)
 app.include_router(user_router)
-app.include_router(comment_router)
