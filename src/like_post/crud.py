748fba184105c7f9ba86142320b7b9a423ee46c8
VadimRight <vadimright@mail.ru>
2024-03-25 15:16:17 +0300

    Fit: delete func for like for post

--- src/like_post/crud.py
+++ src/like_post/crud.py
@@ -1,8 +1,10 @@
 from uuid import uuid4
 from fastapi import Depends
+from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_session
+from src.like_post.schemas import LikeSchema
 from src.models.models import Like_For_Post
 from src.verif import get_id_from_token
 from src.auth.oauth import oauth2_scheme
@@ -14,3 +16,10 @@ async def create_like_post(post_id, session: AsyncSession = Depends(get_session)
         session.add(like_for_post)
         await session.flush
         await session.refresh(like_for_post)
+        return LikeSchema.model_validate(like_for_post)
+    
+
+async def delete_like_post(post_id, session: AsyncSession = Depends(get_session), token: str = Depends(oauth2_scheme)):
+    async with session.begin():
+        query = delete(Like_For_Post).where(post_id == post_id)
+        await session.execute(query)
