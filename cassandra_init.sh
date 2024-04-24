6979e5b661a93aa2e1750762ea10f55460129610
VadimRight <vadimright@mail.ru>
2024-04-24 22:24:45 +0300

    Build: now cassandra is inited in anothoer container

--- cassandra_init.sh
+++ cassandra_init.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env bash
-sleep 5
+#!/bin/bash
+# Wait until Cassandra is ready to accept connections
+until printf "" 2>>/dev/null >>/dev/tcp/cassandra_instagram/9042; do 
+    sleep 5;
+    echo "Waiting for Cassandra...";
+done
 
-echo "Cassandra is up - executing commands"
-cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS fastapiinstagram WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
-cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS fastapiinstagram.image (id uuid PRIMARY KEY, item_id uuid, path varchar, user_id uuid);"
+# Initialization commands
+cqlsh -e "CREATE KEYSPACE IF NOT EXISTS fastapiinstagram WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};"
+cqlsh -e "CREATE TABLE IF NOT EXISTS fastapiinstagram.image (id uuid PRIMARY KEY, item_id uuid, path varchar, user_id uuid);"
 
-echo "Keyspace and Tables created."
