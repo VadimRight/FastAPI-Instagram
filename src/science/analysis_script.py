81f9affe7341d1432e7ff7615831c1bafa859f09
VadimRight <vadimright@mail.ru>
2024-04-26 01:48:27 +0300

    commit for debugging

---
+++ src/science/analysis_script.py
@@ -0,0 +1,7 @@
+from time_decorator import analysis_data
+import matplotlib.pyplot as plt
+
+print(analysis_data)
+plt.plot(analysis_data)
+plt.plot(analysis_data, 'o')
+plt.show()
--- src/science/time_decorator.py
+++ src/science/time_decorator.py
@@ -1,6 +1,9 @@
 from functools import wraps
 from time import time
 
+analysis_data = []
+analysis_data_output = []
+
 def time_decorator(func):
     @wraps(func)
     async def wrapped(*args, **kw):
@@ -9,8 +12,13 @@ def time_decorator(func):
         time_end = time()
         time_execution = time_end - time_start
         time_execution_milliseconds = time_execution * 1000
+        analysis_data.append(time_execution_milliseconds)
+        global analysis_data_output
+        analysis_data_output = analysis_data
+        print(analysis_data_output)
         print('func:%r args:[%r, %r] took: %2.4f millisec' % \
         (func.__name__, args, kw, time_execution_milliseconds))
         return result
     return wrapped
 
+print(analysis_data_output)
