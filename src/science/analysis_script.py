9f3e8c6f8b7fd7209fd1a5b45b0e3c9ee5b485d6
VadimRight <vadimright@mail.ru>
2024-04-26 19:17:30 +0300

    Fit: now graph is more informative

--- src/science/analysis_script.py
+++ src/science/analysis_script.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open('./src/science/data.txt', 'r') as file:
     data = file.read()
@@ -7,8 +8,27 @@ with open('./src/science/data.txt', 'r') as file:
 analysis_data = analysis_data[:-1]
 print(analysis_data)
 analysis_data = [float(i) for i in analysis_data]
-
+analysis_data = [int(i) for i in analysis_data]
 analysis_data = analysis_data[2:]
-plt.plot(analysis_data)
-plt.show()
 
+
+x = range(1, len(analysis_data) + 1)
+
+
+# Create the plot
+plt.figure(figsize=(10, 6))
+plt.plot(x, analysis_data, marker='o', linestyle='-', color='b')
+plt.title('Analysis Data Plot')
+plt.xlabel('Times')
+plt.ylabel('Execution Time')
+plt.grid(True)
+plt.ylim(0, 50)  # Ensure y-axis starts at 0 and has some space above the max value
+plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+
+
+for i, value in enumerate(analysis_data):
+    plt.annotate(value, (x[i], analysis_data[i]), textcoords="offset points", xytext=(0,10), ha='center')
+
+
+# Show the plot
+plt.show()
