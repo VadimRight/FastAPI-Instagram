999a2c369a8728ee941e31b7863fc9569efa1310
VadimRight <vadimright@mail.ru>
2024-04-28 19:06:30 +0300

    Build: now time decorator shows only list of time executions

--- src/science/time_decorator.py
+++ src/science/time_decorator.py
@@ -11,16 +11,18 @@ def time_decorator(func):
         time_end = time()
         time_execution = time_end - time_start
         time_execution_milliseconds = time_execution * 1000
+
         analysis_data.append(time_execution_milliseconds)
         analysis_data_output = [str(i) for i in analysis_data]
-        print(analysis_data_output)
+
         with open('./data/data.txt', 'r') as file:
             lines = file.readlines()
         with open('./data/data.txt', 'w') as file:
             for item in analysis_data_output:
-                file.write(item + '\n')
-        print('func:%r args:[%r, %r] took: %2.4f millisec' % \
-        (func.__name__, args, kw, time_execution_milliseconds))
+                file.write(item + ' ')
+
+        with open('./data/data.txt', 'r') as file:
+            print(file.read())
         return result
     return wrapped
 
